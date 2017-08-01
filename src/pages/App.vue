e4b0616166b3a040ba4641814cfadffb85e47ad0
zhangzc <mrzzhichao@gmail.com>
2017-08-01 14:31:48 +0800

    online

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -5,6 +5,17 @@
         </div>
     </div>
 </template>
+<script>
+    (function() {
+            var _hmt = _hmt || [];
+            (function() {
+                var hm = document.createElement("script");
+                hm.src = "//hm.baidu.com/hm.js?4f816d475bb0b9ed640ae412d6b42cab";
+                var s = document.getElementsByTagName("script")[0];
+                s.parentNode.insertBefore(hm, s);
+            })();
+    })();
+</script>
 <style>
 .we {
     position: relative;
